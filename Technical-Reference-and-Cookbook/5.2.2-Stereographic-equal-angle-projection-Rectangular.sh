7fd1c1708465ae17b8b3be9b6c7688742e8c61cb
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-07-04 13:27:53 +0200

    fix MAP_ANNOT_OBLIQUE, width

--- Technical-Reference-and-Cookbook/5.2.2-Stereographic-equal-angle-projection-Rectangular.sh
+++ Technical-Reference-and-Cookbook/5.2.2-Stereographic-equal-angle-projection-Rectangular.sh
@@ -1,6 +1,6 @@
-gmtset OBLIQUE_ANNOTATION 30  
+gmtset MAP_ANNOT_OBLIQUE 30  
 
-pscoast -R-25/59/70/72r -JS10/90/11c -B30g10/5g5 -Dl -A250 -Glightgray -Wthinnest  > /tmp/map.ps
+pscoast -R-25/59/70/72r -JS10/90/24c -B30g10/5g5 -Dl -A250 -Glightgray -Wthinnest  > /tmp/map.ps
 
 gv /tmp/map.ps
 
