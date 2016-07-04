9935c4b7fc53ea102c081e06e819807cfec72f31
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-07-04 12:00:59 +0200

    Some comments on the -B flag

--- Technical-Reference-and-Cookbook/4.1.1-Calendar-time-coordinates.sh
+++ Technical-Reference-and-Cookbook/4.1.1-Calendar-time-coordinates.sh
@@ -3,6 +3,11 @@ gmtset TIME_WEEK_START          Sunday
 gmtset FORMAT_CLOCK_MAP         hh # also try: hham
 gmtset FORMAT_TIME_PRIMARY_MAP  full  
 
+# -B
+#    K: ISO week day (starting with parameter TIME_WEEK_START, see gmtset above)
+#    k: ?
+#    H: Hour, format using parameter FORMAT_CLOCK_MAP (see gmtset above)
+#    h: Hour, plot with 2 digits
 psbasemap -R2001-9-24T/2001-09-29T/T06:59:59/T15:00:01 -JX15c/-8c -Ba1Kf1kg1d/a1Hg1hWsNe  > /tmp/plot.ps
 
 gv /tmp/plot.ps
