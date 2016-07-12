b257bce0dbe5bfb969800af72492847d75a490f4
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-07-12 22:34:21 +0200

    nc dir IS used

--- SRTM/earthexplorer.usgs.gov-zip_2_bil_2_nc.pl
+++ SRTM/earthexplorer.usgs.gov-zip_2_bil_2_nc.pl
@@ -17,6 +17,7 @@
 #
 #  2016-07-11: The following is not true anymore (the
 #  functionality is commented out)
+#  2016-07-12: well, it IS true...
 #
 #   Finally, xyz2grd (of the GMT tool suite) is used to
 #   convert the .bil file into the netCDF .nc file which
@@ -31,11 +32,11 @@ die usage() if (@ARGV < 2);
 
 my $zip_dir     = shift;
 my $bil_hdr_dir = shift;
-# 2016-07-11: my $nc_dir  = shift;
+my $nc_dir  = shift;
 
 die "$zip_dir     is not a directory" unless -d $zip_dir;
 die "$bil_hdr_dir is not a directory" unless -d $bil_hdr_dir;
-# 2016-07-11: die  "$nc_dir is not a directory" unless -d $nc_dir;
+die "$nc_dir is not a directory"      unless -d $nc_dir;
 
 for my $zip_file_with_path (glob ("$zip_dir/*.zip")) {
 
@@ -62,49 +63,50 @@ for my $zip_file_with_path (glob ("$zip_dir/*.zip")) {
 }
 
 # 2016-07-11 Don't convert bil file into nc file anymore (exctract hdr instead, see above)
-exit 0;
+# 2016-07-12: exit 0;
 
 # 2016-07-11 (Commented out the functionality to convert bil files to nc files)
-# for my $bil_file (glob ("$bil_dir/*.bil")) {
-# 
-#   my ($file_without_suffix) = fileparse($bil_file, '.bil');
-# 
-#   my $nc_file_with_path = "$nc_dir/$file_without_suffix.nc";
-# 
-#   if (-f $nc_file_with_path) {
-#     print "$nc_file_with_path already exists\n";
-#     next;
-#   }
-# 
-#   if ($file_without_suffix =~ /^(n|s)(\d\d)_(e|w)(\d\d\d)_1arc_v3$/) {
-# 
-#     my $ns = $1;
-#     my $ns_val = $2;
-#     my $we = $3;
-#     my $we_val = $4;
-# 
-#     my ($x_from, $x_to, $y_from, $y_to);
-#     if ($ns eq 'n') {
-#       $y_from = int $ns_val;
-#       $y_to   = $y_from + 1;
-#     }
-#     else {die "TODO south files!"};
-#     if ($we eq 'e') {
-#       $x_from = int $we_val;
-#       $x_to   = $x_from + 1;
-#     }
-#     else {
-#       $x_from = - int $we_val;
-#       $x_to   =   $x_from + 1;
-#     };
-# 
-#     print readpipe ("xyz2grd $bil_file -G$nc_file_with_path -I1s -di-32768 -R$x_from/$x_to/$y_from/$y_to -ZTLh");
-# 
-#   }
-#   else {
-#     die "$file_without_suffix didn't match";
-#   }
-# }
+# 2016-07-12 (Well, nc files ARE needed)
+for my $bil_file (glob ("$bil_hdr_dir/*.bil")) {
+
+  my ($file_without_suffix) = fileparse($bil_file, '.bil');
+
+  my $nc_file_with_path = "$nc_dir/$file_without_suffix.nc";
+
+  if (-f $nc_file_with_path) {
+    print "$nc_file_with_path already exists\n";
+    next;
+  }
+
+  if ($file_without_suffix =~ /^(n|s)(\d\d)_(e|w)(\d\d\d)_1arc_v3$/) {
+
+    my $ns = $1;
+    my $ns_val = $2;
+    my $we = $3;
+    my $we_val = $4;
+
+    my ($x_from, $x_to, $y_from, $y_to);
+    if ($ns eq 'n') {
+      $y_from = int $ns_val;
+      $y_to   = $y_from + 1;
+    }
+    else {die "TODO south files!"};
+    if ($we eq 'e') {
+      $x_from = int $we_val;
+      $x_to   = $x_from + 1;
+    }
+    else {
+      $x_from = - int $we_val;
+      $x_to   =   $x_from + 1;
+    };
+
+    print readpipe ("xyz2grd $bil_file -G$nc_file_with_path -I1s -di-32768 -R$x_from/$x_to/$y_from/$y_to -ZTLh");
+
+  }
+  else {
+    die "$file_without_suffix didn't match";
+  }
+}
 
 sub usage {
 
